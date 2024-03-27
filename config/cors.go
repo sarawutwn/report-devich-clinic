d7c854753c797896ca39bb49bd3390169696456e
ishikami senku <>
2024-03-27 21:01:38 +0700

    Update cors.go

--- config/cors.go
+++ config/cors.go
@@ -7,7 +7,7 @@ import (
 var CorsConfigDefault = cors.Config{
 	Next: nil,
 	// AllowOrigins: "*",
-	AllowOrigins:     "http://localhost:5173, http://127.0.0.1:5173, http://localhost:3000, http://127.0.0.1:3000, https://jbrvgcqr-3000.asse.devtunnels.ms",
+	AllowOrigins:     "http://localhost:5173, http://127.0.0.1:5173, http://localhost:3000, http://127.0.0.1:3000, https://react-devich-clinic.onrender.com/",
 	AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
 	AllowHeaders:     "",
 	AllowCredentials: false,
