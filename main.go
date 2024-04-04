675c9218edbc67a71c03b90e70bf786098f93657
ishikami senku <>
2024-04-05 00:17:40 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	GoCache "backend-app/cache/go-cache"
+	"fmt"
+	"time"
 
 	"backend-app/database"
 	"backend-app/router"
@@ -10,6 +12,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/robfig/cron"
 )
 
 func main() {
@@ -18,11 +21,11 @@ func main() {
 	app := fiber.New(fiber.Config{
 		// Prefork: true,
 	})
-	// c := cron.New()
-	// c.AddFunc("@every 110s", func() {
-	// 	fmt.Println(time.Now())
-	// })
-	// c.Start()
+	c := cron.New()
+	c.AddFunc("@every 15m", func() {
+		fmt.Println(time.Now())
+	})
+	c.Start()
 	app.Use(cors.New(Config.CorsConfigDefault))
 	router.SetupRoutes(app)
 	app.Listen(":10000")
